5d8d749568d7353bccb25bd8a293f4bd8015f338
saikat8509 <mailmetosaikat007@gmail.com>
2025-06-15 21:29:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
+from urllib.parse import quote_plus
 
-# Bot token @Botfather
+# Bot token from @BotFather
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 # Your API ID from my.telegram.org
@@ -9,13 +10,24 @@ API_ID = int(os.environ.get("API_ID", "23584757"))
 # Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "ac9926d2cb8acc38413f5e93881fd514")
 
-# Your Owner / Admin Id For Broadcast 
+# Your Owner / Admin ID for Broadcast
 ADMINS = int(os.environ.get("ADMINS", "7616562927"))
 
-# Your Mongodb Database Url
-# Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://creazysaikat:Saikat9735@cluster0.y2ku5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+# MongoDB credentials
+MONGO_USERNAME = os.environ.get("MONGO_USERNAME", "creazysaikat")
+MONGO_PASSWORD = os.environ.get("MONGO_PASSWORD", "Saikat9735")
+MONGO_CLUSTER = os.environ.get("MONGO_CLUSTER", "cluster0.y2ku5.mongodb.net")
+
+# Encode username and password
+encoded_username = quote_plus(MONGO_USERNAME)
+encoded_password = quote_plus(MONGO_PASSWORD)
+
+# Use standard mongodb:// URI to avoid SRV DNS issues
+DB_URI = os.environ.get("DB_URI", f"mongodb://{encoded_username}:{encoded_password}@{MONGO_CLUSTER}:27017/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
 
-# If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
-ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
+# Target channel ID or username for forwarding content
+TARGET_CHANNEL = os.environ.get("TARGET_CHANNEL", "")  # e.g., "-1001234567890" or "@YourChannel"
+
+# Enable/disable error messages in personal chats
+ERROR_MESSAGE = bool(os.environ.get("ERROR_MESSAGE", True))
